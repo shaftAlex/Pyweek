bd3ad783c19dee92a88bb1b0380b69a7b6c61b96
shaftAlex <alexanderscriminger@gmail.com>
2024-03-21 16:43:46 -0400

    Default textures

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ class Game(Entity):
 
 		self.sun = DirectionalLight()
 		self.sun.look_at(Vec3(1, -1, -1))
-		self.sky = Sky()
-		self.ground = Entity(model='plane', collider='box', scale=128)
+		self.sky = Sky(texture='skybox')
+		self.ground = Entity(model='plane', collider='box', scale=128, texture='default', texture_scale=(32, 32))
 
 		return
 
